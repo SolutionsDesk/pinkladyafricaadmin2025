4caaaacadbe0d1aca29585a47c5f80fb2ca8f92a
Web Solutions Desk <84079855+SolutionsDesk@users.noreply.github.com>
2025-10-14 14:38:36 +0200

    Added material tailwind css 3 to the design build for the backend

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,11 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
+// Import Material Tailwind
+import { initMaterialTailwind } from '@material-tailwind/html';
+
 window.Alpine = Alpine;
 
+// Initialize both libraries
+initMaterialTailwind();
 Alpine.start();
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './node_modules/@material-tailwind/html/**/*.{js,jsx,ts,tsx}', // <-- Add this line
     ],
 
     theme: {
