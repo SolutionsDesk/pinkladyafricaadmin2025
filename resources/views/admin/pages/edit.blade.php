8cf2d45a8f74946ef058ddb408578764f0999258
Web Solutions Desk <84079855+SolutionsDesk@users.noreply.github.com>
2025-10-15 22:05:15 +0200

    Add country recognition to edit view so country can be identified

--- resources/views/admin/pages/edit.blade.php
+++ resources/views/admin/pages/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            Edit Page: {{ $page->title }}
+            Edit Page ({{ strtoupper($country_code) }}): {{ $page->title }}
         </h2>
     </x-slot>
 
